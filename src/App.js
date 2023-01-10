1613674e99c59ecad12d2bb4f65c7228048ed80c
Isaiah <idlinares@ufl.edu>
2023-01-10 09:54:01 -0500

    Cleaning up Code

--- src/App.js
+++ src/App.js
@@ -6,14 +6,6 @@ import MovieCard from './MovieCard'
 
 const API_URL = 'https://omdbapi.com?apikey=49e0236e';
 
-const movie1 = {
-    "Title": "Italian Spiderman",
-    "Year": "2007",
-    "imdbID": "tt2705436",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg"
-};
-
 const App = () => {
 
     const [movies, setMovies] = useState([]);
